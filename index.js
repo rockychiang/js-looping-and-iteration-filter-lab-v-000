91b80616cb8d5fa388c001a2f1a51259b81efaeb
Rocky Chiang <rocky@chem.ucla.edu>
2018-05-01 05:22:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ function findMatching(array, name) {
   return array.filter(function(n) {return n.toLowerCase() === name.toLowerCase()})
 }
 
-function fuzzyMatch() {
-  
+function fuzzyMatch(array, initial) {
+  return array.filter(function(n){return n[0] === initial})  
 }
 
 function matchName() {
